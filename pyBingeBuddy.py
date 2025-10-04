4e0fc8e25dca8f515f9ed5a439691962b40fa21f
skeeven <murraysc@gmail.com>
2025-10-04 02:20:11 -0600

    added database tests #4

--- pyBingeBuddy.py
+++ pyBingeBuddy.py
@@ -178,7 +178,7 @@ st.markdown(("✅ " if sqlite_ok else "❌ ") + sqlite_msg)
 
 try:
     conn = sqlitecloud.connect(sqlite_url)
-    # init_db(conn)
+    init_db()
     conn.execute("SELECT 1;")  # sanity probe
     st.success("Connected to SQLite Cloud.")
 except Exception as e:
